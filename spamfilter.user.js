c9d0b3648a20ffa8867ccd1ad7159393fdaa631c
htomasz <htomasz@gmail.com>
2017-01-16 13:17:00 +0100

    Update spamfilter.user.js

--- spamfilter.user.js
+++ spamfilter.user.js
@@ -44,8 +44,8 @@ var setup = function() {
 			filteredData[key] = data[key];
 		}
 		renderData_orig(filteredData, element, likelyWereOldMsgs);
-	}
-}
+	};
+};
 
 //PLUGIN END //////////////////////////////////////////////////////////
 
